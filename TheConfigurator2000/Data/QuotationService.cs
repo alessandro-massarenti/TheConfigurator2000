f9042d1ea3e43ff38ddd91ba54fbb56b779aa69f
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2021-09-26 00:34:17 +0200

    Funziona tutto, solo che non si può aggiungere due volte lo stesso prodotto. Va risolto

--- TheConfigurator2000/Data/QuotationService.cs
+++ TheConfigurator2000/Data/QuotationService.cs
@@ -9,25 +9,18 @@ namespace TheConfigurator2000.Data
     public class QuotationService : IQuotationService
     {
 
-        public void AddProductToQuotation(Product product,Quotation quotation)
-        {
-            using var context = new Context.AppDbContext();
-
-            context.Quotations.Find(quotation.Id).Products.Add(product);
-            context.SaveChanges();
-        }
 
         public void AddQuotation(Quotation quotation)
         {
-
             if (quotation.Id == Guid.Empty)
             {
-                var id = Guid.NewGuid();
-                quotation.Id = id;
+                quotation.Id = Guid.NewGuid();
             }
-            using var context = new Context.AppDbContext();
-            context.Quotations.Add(quotation);
-            context.SaveChanges();
+            using (var context = new Context.AppDbContext())
+            {
+                context.Quotations.Add(quotation);
+                context.SaveChanges();
+            }
         }
 
         public void DeleteQuotation(Guid id)
@@ -40,52 +33,69 @@ namespace TheConfigurator2000.Data
         public Quotation GetQuotation(Guid id)
         {
             using var context = new Context.AppDbContext();
-
-
             //var q = context.Quotations.First();
             ////List<TheConfigurator2000.Classes.Product> products = context.Quotations.First().Products.ToList();
 
             ////Carica i prodotti
             //context.Entry(q).Collection(s => s.Products).Load();
 
-            var q = context.Quotations.SingleOrDefault(q => q.Id == id);
-            if (q != null)
-            context.Entry(q).Collection(s => s.Products).Load();
+            var quotation = context.Quotations.SingleOrDefault(q => q.Id == id);
+            if (quotation != null)
+            context.Entry(quotation).Collection(s => s.Products).Load();
 
-            return q;
+            return quotation;
         }
 
         public List<Quotation> GetQuotations()
         {
-            using var context = new Context.AppDbContext();
+            using (var context = new Context.AppDbContext()){ 
 
-            List<Quotation> q = context.Quotations.ToList();
-            foreach (var x in q)
-            {
-                context.Entry(x).Collection(s => s.Products).Load();
+                List<Quotation> quotations = context.Quotations.ToList();
+                foreach (var x in quotations)
+                {
+                    context.Entry(x).Collection(s => s.Products).Load();
+                }
+                return quotations;
             }
-            return q;
-        }
-
-        public void RemoveProductFromQuotation(Product product, Quotation quotation)
-        {
-            quotation.Products.Remove(product);
-            UpdateQuotation(quotation);
         }
 
         public void UpdateQuotation(Quotation quotation)
         {
 
-            using var context = new Context.AppDbContext();
+            using( var context = new Context.AppDbContext()) {
 
-            var oldQuotation = context.Quotations.FirstOrDefault(q => q.Id == quotation.Id);
-
+                var oldQuotation = context.Quotations.Find(quotation.Id);
 
             oldQuotation.Name = quotation.Name;
-            oldQuotation.Products = quotation.Products;
 
             context.SaveChanges();
+        }
 
+        }
+
+        public void AddProductToQuotation(Product product, Quotation quotation)
+        {
+            using( var context = new Context.AppDbContext()){
+
+                context.Quotations.Find(quotation.Id).Products.Add(product);
+
+                context.SaveChanges();
+            }
+        }
+
+        public void RemoveProductFromQuotation(Product product, Quotation quotation)
+        {
+            using (var context = new Context.AppDbContext())
+            {
+                var quotationInDb = context.Quotations.Find(quotation.Id);
+                context.Entry(quotationInDb).Collection(s => s.Products).Load();
+                quotationInDb.Products.Remove(context.Products.Find(product.Id));
+
+                context.SaveChanges();
+
+
+
+            }
         }
     }
 }
